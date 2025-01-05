9d1d0ddc3c1ffbc6317a0b564f743baa20c0c6e6
OTIS <72382663+1domybest@users.noreply.github.com>
2025-01-05 17:04:49 +0900

    주석 추가
    주석 추가

--- Sources/CustomBottomSheetLibrary/BottomSheetManager/CustomUIKitBottomSheetOption.swift
+++ Sources/CustomBottomSheetLibrary/BottomSheetManager/CustomUIKitBottomSheetOption.swift
@@ -11,28 +11,51 @@ import UIKit
 
 @MainActor
 public struct CustomUIKitBottomSheetOption: Sendable {
+    /// 바텀시트의 고유 아이디
     public var pk: UUID
-    public var someView: AnyView
-    public var sheetMode: UIModalPresentationStyle = .custom
-    // 핸들 가능 모드 custom, automatic , popover, pageSheet, formSheet
-    // 핸들 불가능 모드 currentContext, fullScreen, overCurrentContext, overFullScreen,
     
+    /// 바텀시트 내부에 사용한 SwiftUIView
+    public var someView: AnyView
+    
+    /// 바텀시트 모드
+    /// 핸들 가능 모드 custom, automatic , popover, pageSheet, formSheet
+    /// 핸들 불가능 모드 currentContext, fullScreen, overCurrentContext, overFullScreen,
+    public var sheetMode: UIModalPresentationStyle = .custom
+    
+    /// 시트 높이
     public var sheetHeight: CGFloat = UIScreen.main.bounds.height / 2
+    
+    /// 드레그 가능 여부
     public var dragAvailable: Bool = true
+    
+    /// 외부 뒷배경 터치시 닫힘유무
     public var availableOutTouchClose: Bool = true
+    
+    /// 시트 상단에 손잡이 노출 여부
     public var showHandler: Bool = true
     
+    /// 뒷배경 색
     public var backgroundColor: Color = Color.black.opacity(0.5)
+    
+    /// 시트 상단 손잡이 색
     public var handlerColor: Color = Color.gray
+    
+    /// 시트 컬러
     public var sheetColor: Color = Color.white
+    
+    /// 시트 그림자 컬러
     public var sheetShadowColor: Color = Color.gray
     
-    
+    /// 최소 높이
     public var minimumHeight:CGFloat = 100
+    
+    /// 최대 높이
     public var maximumHeight:CGFloat = UIScreen.main.bounds.height
     
+    /// someView내부에 키보드 사용유무
     public var hasKeyboard: Bool = false
     
+    /// 커스텀 핸들 노출유무 아래 나머지 조건은 전체모드임
     public var availableHasHandle: Bool {
         return showHandler && (sheetMode == .custom ||
                                sheetMode == .automatic ||
@@ -41,9 +64,13 @@ public struct CustomUIKitBottomSheetOption: Sendable {
                                sheetMode == .formSheet )
     }
     
+    /// 스크롤링시 콜백 함수
     public var onScrolling: ((Double) -> Void)? = { _ in }
+    
+    /// 스크롤링 종료시 콜백함수
     public var onEndTouchScrolling: ((Double, Bool) -> Void)? = { _,_ in }
     
+    /// 시트가 닫혔을시 콜백함수
     public var onDismiss: (() -> Void)? = {}
     
     
